5feff0431274d05c5c343b9b3826c572217922d3
Dan Duta <danduta23@gmail.com>
2019-10-26 18:30:02 +0300

    buying vale

--- src/com/etc/Bot.java
+++ src/com/etc/Bot.java
@@ -92,6 +92,7 @@ public class Bot
                     if (!valbzFairValue.first.equals(0) && !valbzFairValue.second.equals(0)) {
                         System.out.println("Am intrat pe primul!!!!!!");
                         if ((valbzFairValue.first - container.buying.get(0).getPrice()) > 10) {
+                            System.out.println("Am intrat pe al doilea!!!!!!");
                             System.out.println("BUYING VALE");
                             orderStack.addLast(new Order(lastOrderId++, "ADD", "VALE", true,
                                     10, container.selling.get(0).getPrice()));
