747a59475e8de4ef6e5c14018ead969414540198
Dan Duta <danduta23@gmail.com>
2019-10-26 11:12:33 +0300

    sample bot

---
+++ src/com/etc/Configuration.java
@@ -0,0 +1,32 @@
+package com.etc;
+
+import java.lang.*;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.Socket;
+
+public class Configuration {
+    String exchange_name;
+    int    exchange_port;
+    /* 0 = prod-like
+       1 = slow
+       2 = empty
+    */
+    final Integer test_exchange_kind = 2;
+    /* replace REPLACEME with your team name! */
+    final String  team_name          = "BEESKNEES";
+
+    Configuration(Boolean test_mode) {
+        if(!test_mode) {
+            exchange_port = 20000;
+            exchange_name = "production";
+        } else {
+            exchange_port = 20000 + test_exchange_kind;
+            exchange_name = "test-exch-" + this.team_name;
+        }
+    }
+
+    String  exchange_name() { return exchange_name; }
+    Integer port()          { return exchange_port; }
+}
